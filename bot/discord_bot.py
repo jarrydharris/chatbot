87869722407f40484c5a498d163a4e05258a9d72
Jarryd Harris <jhar0383@uni.sydney.edu.au>
2021-10-09 13:36:35 +1100

    bot able to connect, logging successful

--- bot/discord_bot.py
+++ bot/discord_bot.py
@@ -8,11 +8,15 @@ LOG_PATH = "./logs/discord.log"
 class DiscordBot(discord.Client):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        # Logger setup
         self.logger = logging.getLogger('discord')
         self.logger.setLevel(logging.DEBUG)
-        self.handler = logging.FileHandler(filename=LOG_PATH, encoding='utf-8', mode='w+')
+        self.handler = logging.FileHandler(filename=LOG_PATH, encoding='utf-8', mode='a')
         self.handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
         self.logger.addHandler(self.handler)
+        self.logger.info("STARTING SESSION.")
+
+        # Get discord api token from environment variable
         self.token = os.getenv('DISCORD_TOKEN')
         if self.token is None:
             print("ERROR:")
@@ -20,13 +24,13 @@ class DiscordBot(discord.Client):
             self.logger.debug("ERROR: token was None.")
             print("\tExiting...")
             sys.exit()
-        
 
-        
-
+    def get_token(self):
+        return self.token
 
     async def on_ready(self):
         print('We have logged in as {0.user}'.format(self))
+        self.logger.info("CONNECTED.")
 
     async def on_message(self, message):
         if message.author == self.user:
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from bot.discord_bot import DiscordBot
 
 if __name__ == "__main__":
     client = DiscordBot()
+    client.run(client.get_token())
