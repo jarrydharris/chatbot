d3e37b704d247bad83810148fce0320dea4c88cb
Jarryd Harris <jhar0383@uni.sydney.edu.au>
2021-10-10 17:31:50 +1100

    bot will how have a conversation when it is mentioned

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from model.model import DialogueModel
 if __name__ == "__main__":
 
     # Set up model
-    print("Setting up model")
+    print("Setting up tokenizer")
     model_size = "medium"
     tokenizer = AutoTokenizer.from_pretrained(f"microsoft/DialoGPT-{model_size}")
+    print("Setting up model")
     model = AutoModelForCausalLM.from_pretrained(f"microsoft/DialoGPT-{model_size}")
     bot = DialogueModel(model, tokenizer)
-    print("Done.")
+    print("Done.")    
     
     # Start Discord Client
     print("Starting Discord Client")
--- model/model.py
+++ model/model.py
@@ -11,16 +11,21 @@ class DialogueModel:
         self.tokenizer = tokenizer
         # TODO: Handle large chat histories
 
-        # TODO: Blank tensors
-        self.bot_input_ids = torch.Tensor()
-        self.chat_history_ids = torch.Tensor()
+        self.bot_input_ids = None
+        self.chat_history_ids = None
 
     def chat(self, message: str) -> str:
         new_user_input_ids = self.tokenizer.encode(message + self.tokenizer.eos_token, 
             return_tensors='pt')
-        self.bot_input_ids = torch.cat([self.chat_history_ids, new_user_input_ids], dim=-1) if len(self.bot_input_ids) > 0 else new_user_input_ids
-        chat_history_ids = self.model.generate(self.bot_input_ids, max_length=10000, pad_token_id=self.tokenizer.eos_token_id)
+        self.bot_input_ids = torch.cat([self.chat_history_ids, new_user_input_ids], dim=-1) if self.bot_input_ids is not None else new_user_input_ids
+        self.chat_history_ids = self.model.generate(self.bot_input_ids, max_length=10000, pad_token_id=self.tokenizer.eos_token_id, temperature=0.6, repetition_penalty=1.3)
 
-        return "DialoGPT: {}".format(self.tokenizer.decode(chat_history_ids[:, self.bot_input_ids.shape[-1]:][0], skip_special_tokens=True))
+        # print("Message: ", message)
+        # print("new_user_input_ids: ", str(new_user_input_ids))
+        # print("bot_input_ids: ", str(self.bot_input_ids))
+        # print("chat_history_ids: ", str(self.chat_history_ids))
+
+
+        return "{}".format(self.tokenizer.decode(self.chat_history_ids[:, self.bot_input_ids.shape[-1]:][0], skip_special_tokens=True))
 
 
